25902af6e31e1a462b863f2b23304f3fd53b2a68
Christoph Gruener <christophgruener@me.com>
2019-02-25 22:31:49 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,13 +3,12 @@ class Post
   attr_accessor :author, :title
   @all = []
 
-  
-
   def initialize(title)
     @title = title
     # binding.pry
     @@all << self
   end
+
 
   def self.all
     @all
